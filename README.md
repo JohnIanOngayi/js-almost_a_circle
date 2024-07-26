ba47722ff19a2e3c56b6c47efa042b088942accc
JohnIanOngayi <ongayijohnian@gmail.com>
2024-07-26 15:18:28 +0300

    feat: added a Rectangle class
    Class defines a rectangle object and inherits from class Base

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nodejs-practice
-Wo ich js lerne
+
+## Wo ich js lerne
+This is a repo based on ALX's python-almost_a_circle project done in js
---
+++ models/rectangle.js
@@ -0,0 +1,44 @@
+#!/usr/bin/node
+
+const Base = require("./base.js");
+
+/**
+ * module defines a class for a Rectangle
+ * @param {number} width: defines rectangle width (private)
+ * @param {number} height: defines rectangle height (private)
+ * @param {number} x: defines horizontal coordinate (private)
+ * @param {number} y: defines vertical coordinate (private)
+ */
+
+class Rectangle extends Base {
+  /**
+   * Class defines Rectangle shape
+   */
+  constructor(width, height, x, y, id) {
+    super(id);
+    if (width == null) {
+      throw new Error("width cannot be null");
+    }
+    if (height == null) {
+      throw new Error("height cannot be null");
+    }
+    try {
+      this.width = width;
+      this.height = height;
+      if (x == null) {
+        this.x = 0;
+      } else {
+        this.x = x;
+      }
+      if (y == null) {
+        this.y = 0;
+      } else {
+        this.y = y;
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+}
+
+module.exports = Rectangle;
